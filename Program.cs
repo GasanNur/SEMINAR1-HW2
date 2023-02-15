55bbc2e202b8abffe9267648411bae5b6cb98178
Gasan Nuriev <C:\Users\Gasan\AppData\Roaming\The Bat!>
2023-02-15 13:07:45 +0300

    new commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт 
+// максимальное из этих чисел.
+
+int Prompt(string message)
+{
+    Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = Prompt ("Ведите 1 число");
+int b = Prompt ("Ведите 2 число");
+int c = Prompt ("Ведите 3 число");
+
+int Max = a;
+if (b > Max) Max = b;
+if (c > Max) Max = c; 
+Console.WriteLine(Max);
